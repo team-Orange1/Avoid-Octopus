a422ddd5ac1878be881c8487a1d70f287deaa449
Shu Araki <hw15a008@oecu.jp>
2017-05-25 18:24:35 +0900

    ジャンプ
    重力とジャンプ力調整必要

--- Assets/Script/New Folder/newPlayer.cs
+++ Assets/Script/New Folder/newPlayer.cs
@@ -1,41 +1,66 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class newPlayer : MonoBehaviour {
+//	// 速度
+//	public Vector2 SPEED = new Vector2(0.05f, 0.05f);
+//	// Use this for initialization
+//	void Start () {
+//		GetComponent<Rigidbody2D> ().freezeRotation = true;
+//	}
+//
+//	// Update is called once per frame
+//	void Update () {
+//		// 移動処理
+//		Move();
+//	}
+//
+//	// 移動関数
+//	void Move(){
+//		// 現在位置をPositionに代入
+//		Vector2 Position = transform.position;
+//		if(Input.GetKey("left")){
+//			// 代入したPositionに対して加算減算を行う
+//			Position.x -= SPEED.x;
+//		} else if(Input.GetKey("right")){ 
+//			// 代入したPositionに対して加算減算を行う
+//			Position.x += SPEED.x;
+//		} else if(Input.GetKey("up")){ // 上キーを押し続けていたら
+//			// 代入したPositionに対して加算減算を行う
+//			Position.y += SPEED.y;
+//		} 
+//		// 現在の位置に加算減算を行ったPositionを代入する
+//		transform.position = Position;
+//	}
+//}
+using UnityEngine;
 using System.Collections;
 
 public class newPlayer : MonoBehaviour {
-	// 速度
-	public Vector2 SPEED = new Vector2(0.05f, 0.05f);
-	// Use this for initialization
-	void Start () {
 
+	// 変数の定義と初期化
+	public float flap = 1000f;
+	public float scroll = 5f;
+	Rigidbody2D rb2d;
+
+	// Updateの前に1回だけ呼ばれるメソッド
+	void Start() {
+		// Rigidbody2Dをキャッシュする
+		rb2d = GetComponent<Rigidbody2D>();
+		GetComponent<Rigidbody2D> ().freezeRotation = true;
 	}
 
-	// Update is called once per frame
-	void Update () {
-		// 移動処理
-		Move();
-	}
+	// シーン中にフレーム毎に呼ばれるメソッド
+	void Update() {
+		// xの正方向にscrollスピードで移動
+		rb2d.velocity = new Vector2(scroll, rb2d.velocity.y);
 
-	// 移動関数
-	void Move(){
-		// 現在位置をPositionに代入
-		Vector2 Position = transform.position;
-		// 左キーを押し続けていたら
-		if(Input.GetKey("left")){
-			// 代入したPositionに対して加算減算を行う
-			Position.x -= SPEED.x;
-		} else if(Input.GetKey("right")){ // 右キーを押し続けていたら
-			// 代入したPositionに対して加算減算を行う
-			Position.x += SPEED.x;
-		} else if(Input.GetKey("up")){ // 上キーを押し続けていたら
-			// 代入したPositionに対して加算減算を行う
-			Position.y += SPEED.y;
-		} else if(Input.GetKey("down")){ // 下キーを押し続けていたら
-			// 代入したPositionに対して加算減算を行う
-			Position.y -= SPEED.y;
+		// スペースキーが押されたら
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			// 落下速度をリセット
+			rb2d.velocity = Vector2.zero;
+			// (0,1)方向に瞬間的に力を加えて跳ねさせる
+			rb2d.AddForce(Vector2.up * flap);//, ForceMode2D.Impulse);
 		}
-		// 現在の位置に加算減算を行ったPositionを代入する
-		transform.position = Position;
 	}
-
-
 }
